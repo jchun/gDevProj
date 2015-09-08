e8f333b2ba435c51e8294563f9a61b02ef6589ce
Joey Chun <jchundev@gmail.com>
2015-09-08 16:06:36 -0700

    levisEventsCoordinator.py

---
+++ levisCalendar/levisEventsCoordinator.py
@@ -0,0 +1,114 @@
+from apiclient import discovery
+import datetime
+import httplib2
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+import os
+
+import levisParser
+savedLocalEvents = levisParser.savedLocalEvents
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/calendar'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Levi\'s Stadium Events Coordinator'
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-quickstart.json')
+
+    store = oauth2client.file.Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatability with Python 2.6
+            credentials = tools.run(flow, store)
+        print 'Storing credentials to ' + credential_path
+    return credentials
+
+def createEvent(service, eventTimeDate, eventTitle, eventURL):
+    eventTimeDate = datetime.date(eventTimeDate[2], eventTimeDate[0], eventTimeDate[1]).isoformat()
+    event = {
+      'summary': eventTitle,
+      'location': '4900 Marie P Bartolo Way, Santa Clara, CA 95054',
+      'description': eventURL,
+      'start': {
+        'date': eventTimeDate,
+        'timeZone': 'America/Los_Angeles',
+      },
+      #'''
+      'end': {
+        'date': eventTimeDate,
+        'timeZone': 'America/Los_Angeles',
+      },
+      #'''
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+  
+
+    gEvent = service.events().insert(calendarId='ahr56gto4a44e4uj2bumer2h5k@group.calendar.google.com', body=event).execute()
+    print 'Event created: %s' % (gEvent.get('htmlLink'))
+ 
+def getEvents(service, numEvents):
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print 'Getting the upcoming %d events' % (numEvents)
+    eventsResult = service.events().list(
+        calendarId='ahr56gto4a44e4uj2bumer2h5k@group.calendar.google.com', timeMin=now, maxResults=numEvents, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    if not events:
+        print 'No upcoming events found.'
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print start, event['summary']
+
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+
+    Creates a Google Calendar API service object and outputs a list of the next
+    10 events on the user's calendar.
+    """
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+    
+    levisParser.main()
+    for (eventTimeDate, eventInfo)  in savedLocalEvents.items():
+        print eventTimeDate 
+        #print eventInfo
+        eventTitle, eventURL = eventInfo
+
+    createEvent(service, eventTimeDate, eventTitle, eventURL)
+    getEvents(service, 10) 
+
+if __name__ == '__main__':
+    main()
+
---
+++ levisCalendar/levisParser.py
@@ -0,0 +1,116 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+'''
+Created by Joseph Chun
+August 27th 2015
+'''
+import argparse
+from bs4 import BeautifulSoup
+import calendar
+import datetime
+import os
+import re
+import time
+import urllib2
+
+calendarPage = 'http://www.levisstadium.com/events/category/tickets/'
+months = {v: k for k,v in enumerate(calendar.month_abbr)}
+savedLocalEvents = {}
+
+def cleanDate(date):
+    dateSplit = date.split()
+    eventMonth = months[dateSplit[0][0:3]]
+    eventDay = int(re.sub('[^0-9]','', date.split()[1]))
+    eventYear = int(dateSplit[2])
+    eventTime = '0:00'
+
+    return (eventMonth, eventDay, eventYear, eventTime)
+
+def parseEvent(eventLink):
+    '''
+    Parse the individual event page
+    '''
+    eventSoup = soupIt(eventLink)
+   
+    titleInfo = eventSoup.findAll('h1', { 'class' : 'page-title' })
+
+    if not titleInfo:
+        print 'No title found, skipping add'
+        
+    else:
+        title = titleInfo[0].string.strip()
+
+    dateInfo = eventSoup.findAll('div', { 'class' : 'date' })
+
+    if not dateInfo:
+        print 'No dates found, skipping add'
+        
+    else:
+        for dateField in dateInfo:
+            date = dateField.findAll('span', { 'class' : 'text' })    
+        date = date[0].string
+    
+    ''' 
+    print '*' * 15
+    print title
+    print date
+    print months[date[0:3]]
+    print eventLink
+    print '*' * 15
+    '''
+
+    date = cleanDate(date)
+
+    parsedEvent = title, eventLink
+    #print parsedEvent
+    #return parsedEvent
+    savedLocalEvents[date] = parsedEvent
+
+
+def parseCalendar(calendarLink):
+    '''
+    Parse the main events page
+    '''
+    calSoup = soupIt(calendarPage)
+    events = calSoup.findAll('article')
+    
+    if not events:
+        print 'No events found'
+        return
+    
+    else:
+        for event in events:
+            links = event.findAll('a', href=True)
+            eventLink = links[1]['href']
+            parseEvent(eventLink)
+            '''@TODO remove following break:'''
+
+def soupIt(url):
+    '''
+    Given a url, generate and return the BeautifulSoup
+    '''
+    try:
+        u = urllib2.urlopen(url, timeout=3)
+        soup = BeautifulSoup(u, 'html.parser')
+        return soup
+    except urllib2.URLError, e:
+        raise NameError('URL Request Error')
+
+def main():
+    parseCalendar(calendarPage)
+
+if __name__ == '__main__':
+    startTime = time.time()
+    
+    #'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--output')
+    parser.add_argument('-v', dest='verbose', action='store_true')
+    args = parser.parse_args()
+    
+    #print 'args: ' + str(args)
+    #'''
+   
+    main()    
+
+    print 'Time taken: ' + str(time.time()-startTime) + ' secs'
